c2dd21609966d1a36f71f085c3aa0f93d1154d47
Jessica Gillan <gillan.jessica@gmail.com>
2025-07-09 19:11:55 -0600

    Update ContactsServicePlugin.m to import contacts_service_fork-Swift.h

--- ios/Classes/ContactsServicePlugin.m
+++ ios/Classes/ContactsServicePlugin.m
@@ -1,8 +1,8 @@
 #import "ContactsServicePlugin.h"
-#if __has_include(<contacts_service/contacts_service-Swift.h>)
-#import <contacts_service/contacts_service-Swift.h>
+#if __has_include(<contacts_service_fork/contacts_service_fork-Swift.h>)
+#import <contacts_service_fork/contacts_service_fork-Swift.h>
 #else
-#import "contacts_service-Swift.h"
+#import "contacts_service_fork-Swift.h"
 #endif
 
 @implementation ContactsServicePlugin
